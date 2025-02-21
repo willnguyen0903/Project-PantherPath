b96aa2c0d44de29d69774fe047096728e25af757
Steven Tran <steventran@Stevens-MacBook-Air.local>
2025-02-21 16:12:10 -0500

    Modify link to the server.

--- backendApi/server.js
+++ backendApi/server.js
@@ -23,5 +23,5 @@ app.get("/marta/schedule", async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}/marta/schedule`);
 });
